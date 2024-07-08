62b7ef3c12a3439d784b5fe1ec9039db3fdfad2c
mehmet <mehmet00arslan@gmail.com>
2024-07-08 22:22:34 +0300

    Fonksiyonlar

---
+++ IIFE.js
@@ -0,0 +1,8 @@
+//? ====================================================
+// ?              FONKSİYONLAR - IIFE
+// ? ====================================================
+
+//**Immediately Invoked Function Expression (IIFE) yani "Hemen Çağrılan Fonksiyon İfadesi" */
+(function(){
+    console.log("Merhaba")
+})()
